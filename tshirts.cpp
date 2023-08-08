6579ede070f2044d66c0c854cd99052f6ff3c4d9
Ajay Kumar <ajay.kumar_9@philips.com>
2023-08-08 17:54:14 +0530

    commit3

--- tshirts.cpp
+++ tshirts.cpp
@@ -6,11 +6,11 @@ using namespace tshirt;
 
 void Test_TShirtSize()
 {
-    assert(tshirt::size(37) == 'S');
-    assert(tshirt::size(38) == 'S');
-    assert(tshirt::size(40) == 'M');
-    assert(tshirt::size(42) == 'L');
-    assert(tshirt::size(43) == 'L');
+    assert(size(37) == 'S');
+    assert(size(38) == 'S');
+    assert(size(40) == 'M');
+    assert(size(42) == 'L');
+    assert(size(43) == 'L');
     std::cout << "All is well (maybe!)\n";
 }
 
