e1b4cd6bf43efe7dc8c460379dbd97a11eb25ec1
Ajay Kumar <ajay.kumar_9@philips.com>
2023-08-08 18:17:05 +0530

    commit9

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,6 +1,24 @@
+//#include "PrintColorMapTest.h"
 #include "PrintColorMapTest.h"
-#include "PrintColorMapTest.h"
+#include "ColorMap.h"
+void test_PrintColorMap()
+{
+    std::vector<colormap::ColorPair> colorMap = colormap::PrintColorMap();
+    int result = colorMap.size();
+    assert(result == 25);
 
+    assert(colorMap[0].number == 1);
+    assert(colorMap[0].majorColor == "White");
+    assert(colorMap[0].minorColor == "Blue");
+
+    assert(colorMap[12].number == 13);
+    assert(colorMap[12].majorColor == "Black");
+    assert(colorMap[12].minorColor == "Green");
+
+    assert(colorMap[24].number == 25);
+    assert(colorMap[24].majorColor == "Violet");
+    assert(colorMap[24].minorColor == "Slate");
+}
 
 int main() {
     
